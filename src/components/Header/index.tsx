f325fd45d72ba4587a185af682fedb01b1a05328
A91y <ayush.agr254@gmail.com>
2024-06-23 23:16:21 +0530

    refactor: Remove unused SVG files and update landing page layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -6,7 +7,8 @@ type Props = {};
 const Header = (props: Props) => {
   return (
     <nav className="text-white fixed top-0 right-0 left-0 w-full h-20 flex justify-between items-center md:px-32 px-8 py-4 ">
-      <div className="text-xl md:text-2xl font-bold">Octasol</div>
+      {/* <div className="text-xl md:text-2xl font-bold"></div> */}
+      <Image src="/octasolLandingLogo.png" alt="logo" width={80} height={80} ></Image>
       <Link
         href="/dashboard"
         className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block"
