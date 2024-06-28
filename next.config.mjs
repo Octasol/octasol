75c213b38edf8020771d99776ecd830530ff53db
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-06-28 11:03:00 +0530

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    remotePatterns: [{
+      protocol:"https",
+      hostname:"avatars.githubusercontent.com"
+    }],
   },
 };
 
