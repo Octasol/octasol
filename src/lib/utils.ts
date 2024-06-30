a7c29c457b1b52f7260e0eccca84b1db47f004c5
A91y <ayush.agr254@gmail.com>
2024-06-30 16:19:25 +0530

    refactor: Update utils.ts to include functions for opening URLs in new tabs/windows

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function openInNewTab(url: string) {
+  const win = window.open(url, "_blank");
+  if (win) win.focus();
+}
+
+export function openInNewWindow(url: string) {
+  window.open(
+    url,
+    "_blank",
+    "toolbar=yes,scrollbars=yes,resizable=yes,width=1000,height=1000, left=500, top=500"
+  );
 }
