c7f7e6c6b76e3ec4e7ba0cae7aca85373ffdc16f
mayank-dev07 <devmayank.hts@gmail.com>
2024-08-03 15:01:48 +0530

    fest: ranking of leaderboard added

--- src/app/(pages)/leaderboard/page.tsx
+++ src/app/(pages)/leaderboard/page.tsx
@@ -10,56 +10,76 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { GET } from "@/config/axios/requests";
+import { cn } from "@/lib/utils";
 
-type Props = {};
+interface Profile {
+  followers: number;
+  forkedRepos: number;
+  forks: number;
+  mergedPullRequests: number;
+  originalRepos: number;
+  pullRequests: number;
+  repositoriesContributedTo: number;
+  stars: number;
+  totalCommits: number;
+  totalIssues: number;
+  User: {
+    githubUsername: string;
+  };
+  points: number;
+}
 
-const Leaderboard = (props: Props) => {
-  const [profile, setProfile] = useState<any>(null);
-  const [topProfiles, setTopProfiles] = useState<any>(null);
+const Leaderboard = () => {
+  const [profile, setProfile] = useState<Profile[]>();
+  const [topProfiles, setTopProfiles] = useState<Profile[]>();
 
   const response = async () => {
     const res = await GET("/devprofile/github/");
     console.log(res);
     setProfile(res);
-    setTopProfiles(res.slice(0, 3));
+    const response = res;
+    const element1 = response?.splice(0, 1, response[1]);
+    response?.splice(1, 1, element1[0]);
+    setTopProfiles(response.slice(0, 3));
   };
 
   useEffect(() => {
     response();
   }, []);
 
-  useEffect(() => {
-    const element1 = topProfiles?.splice(0, 1, topProfiles[1]);
-    topProfiles?.splice(1, 1, element1[0]);
-    setTopProfiles(topProfiles);
-  }, [topProfiles]);
+  useEffect(() => {}, [topProfiles]);
 
   return (
     <>
       <div className="w-full flex justify-center items-center text-3xl font-bold tracking-widest">
         LEADERBOARD
       </div>
-      <div className="w-full h-52 bg-[#315478]">
+      <div className="w-full h-fit ">
         <div className="w-full flex justify-center items-center py-10">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-9/12 place-items-center">
             {topProfiles &&
               topProfiles.map((profile: any, index: number) => (
-                <div
-                  key={index}
-                  className={`p-6 border rounded-lg shadow-lg ${
-                    index === 1
-                      ? "bg-yellow-900 border-yellow-5800"
-                      : "bg-red-900"
-                  }`}
-                >
-                  <div className="text-center font-bold text-xl mb-2">
-                    Rank {index + 1}
-                  </div>
-                  <div className="text-center text-lg mb-2">
-                    {profile.User.githubUsername}
-                  </div>
-                  <div className="text-center text-lg">
-                    Points: {profile.points}
+                <div className="w-8/12 block justify-center items-center relative">
+                  <span className="absolute inset-0 overflow-hidden ">
+                    <span className="absolute inset-0  bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+                  </span>
+                  <div
+                    className={cn(
+                      "relative flex space-x-2 justify-between items-center z-10  bg-zinc-950 py-1 px-5 ring-1 ring-white/10 "
+                    )}
+                  >
+                    <div
+                      key={index}
+                      className="px-6 py-8 flex flex-col justify-center items-center w-full"
+                    >
+                      <div className="text-center font-bold text-xl mb-2">
+                        Rank {index === 0 ? 2 : index === 1 ? 1 : 3}
+                      </div>
+                      <div className="text-center text-lg mb-2">
+                        {profile.User.githubUsername}
+                      </div>
+                    </div>
+                    <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
                   </div>
                 </div>
               ))}
