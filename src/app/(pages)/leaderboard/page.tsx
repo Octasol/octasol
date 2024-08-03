f821cfc1e4a5ae0700715940e3122a9c4d69fa6f
mayank-dev07 <devmayank.hts@gmail.com>
2024-08-03 15:17:16 +0530

    fest: ranking of leaderboard added

--- src/app/(pages)/leaderboard/page.tsx
+++ src/app/(pages)/leaderboard/page.tsx
@@ -37,10 +37,7 @@ const Leaderboard = () => {
     const res = await GET("/devprofile/github/");
     console.log(res);
     setProfile(res);
-    const response = res;
-    const element1 = response?.splice(0, 1, response[1]);
-    response?.splice(1, 1, element1[0]);
-    setTopProfiles(response.slice(0, 3));
+    setTopProfiles([res[1], res[0], res[2]]);
   };
 
   useEffect(() => {
@@ -56,16 +53,17 @@ const Leaderboard = () => {
       </div>
       <div className="w-full h-fit ">
         <div className="w-full flex justify-center items-center py-10">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-9/12 place-items-center">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-9/12 place-items-center">
             {topProfiles &&
               topProfiles.map((profile: any, index: number) => (
-                <div className="w-8/12 block justify-center items-center relative">
+                <div className="w-10/12 block justify-center items-center relative">
                   <span className="absolute inset-0 overflow-hidden ">
                     <span className="absolute inset-0  bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
                   </span>
                   <div
                     className={cn(
-                      "relative flex space-x-2 justify-between items-center z-10  bg-zinc-950 py-1 px-5 ring-1 ring-white/10 "
+                      "relative flex space-x-2 justify-between items-center z-10  bg-zinc-950 py-1 px-5 ring-1 ring-white/10 ",
+                      index === 1 && "scale-125"
                     )}
                   >
                     <div
