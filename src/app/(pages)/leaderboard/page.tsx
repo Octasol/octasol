4fcd12bc00177be2dcc04b5e4316f43efadce698
mayank-dev07 <devmayank.hts@gmail.com>
2024-08-03 16:32:40 +0530

    feat: Rank card initialized

--- src/app/(pages)/leaderboard/page.tsx
+++ src/app/(pages)/leaderboard/page.tsx
@@ -3,90 +3,50 @@ import React, { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
 import { GET } from "@/config/axios/requests";
-import { cn } from "@/lib/utils";
-
-interface Profile {
-  followers: number;
-  forkedRepos: number;
-  forks: number;
-  mergedPullRequests: number;
-  originalRepos: number;
-  pullRequests: number;
-  repositoriesContributedTo: number;
-  stars: number;
-  totalCommits: number;
-  totalIssues: number;
-  User: {
-    githubUsername: string;
-  };
-  points: number;
-}
+import RankCard from "@/components/RankCard";
+import { Profile } from "@/lib/types";
 
 const Leaderboard = () => {
   const [profile, setProfile] = useState<Profile[]>();
-  const [topProfiles, setTopProfiles] = useState<Profile[]>();
 
   const response = async () => {
     const res = await GET("/devprofile/github/");
     console.log(res);
     setProfile(res);
-    setTopProfiles([res[1], res[0], res[2]]);
   };
 
   useEffect(() => {
     response();
   }, []);
 
-  useEffect(() => {}, [topProfiles]);
-
   return (
     <>
       <div className="w-full flex justify-center items-center text-3xl font-bold tracking-widest">
         LEADERBOARD
       </div>
-      <div className="w-full h-fit ">
-        <div className="w-full flex justify-center items-center py-10">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-9/12 place-items-center">
-            {topProfiles &&
-              topProfiles.map((profile: any, index: number) => (
-                <div className="w-10/12 block justify-center items-center relative">
-                  <span className="absolute inset-0 overflow-hidden ">
-                    <span className="absolute inset-0  bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
-                  </span>
-                  <div
-                    className={cn(
-                      "relative flex space-x-2 justify-between items-center z-10  bg-zinc-950 py-1 px-5 ring-1 ring-white/10 ",
-                      index === 1 && "scale-125"
-                    )}
-                  >
-                    <div
-                      key={index}
-                      className="px-6 py-8 flex flex-col justify-center items-center w-full"
-                    >
-                      <div className="text-center font-bold text-xl mb-2">
-                        Rank {index === 0 ? 2 : index === 1 ? 1 : 3}
-                      </div>
-                      <div className="text-center text-lg mb-2">
-                        {profile.User.githubUsername}
-                      </div>
-                    </div>
-                    <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
-                  </div>
-                </div>
-              ))}
+      <div className="w-full h-fit pt-4">
+        <div className="w-full flex flex-col justify-center items-center py-2 md:py-10">
+          <div className="mb-8 md:mb-0 z-20">
+            <RankCard profile={profile && profile[0]} rank={1} />
+          </div>
+          <div className="flex flex-col md:flex-row justify-evenly items-center gap-4 md:gap-32 container place-items-center">
+            <div>
+              <RankCard profile={profile && profile[1]} rank={2} />
+            </div>
+            <div>
+              <RankCard profile={profile && profile[2]} rank={3} />
+            </div>
           </div>
         </div>
       </div>
       <div className="flex justify-center items-center container max-w-5xl">
         <Table>
-          {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
           <TableHeader>
             <TableRow>
               <TableHead className="w-[100px]">Rank</TableHead>
---
+++ src/components/RankCard/index.tsx
@@ -0,0 +1,53 @@
+import { Profile } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  profile: Profile | undefined;
+  rank?: number;
+};
+
+const RankCard = (props: Props) => {
+  console.log(props);
+
+  return (
+    <>
+      {props.profile && (
+        <div className="w-64 block justify-center items-center relative">
+          <span className="absolute inset-0 overflow-hidden ">
+            <span className="absolute inset-0  bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+          </span>
+          <div
+            className={cn(
+              "relative flex space-x-2 justify-between items-center z-10  bg-zinc-950 py-1 px-5 ring-1 ring-white/10 ",
+              props?.rank === 1 ? "scale-110" : "scale-90"
+            )}
+          >
+            <div className="px-4 py-3 flex flex-col justify-center items-center w-full">
+              <div className="w-20 h-20 bg-gray-600 rounded-full">
+                <Image
+                  src={
+                    "https://avatars.githubusercontent.com/" +
+                    props?.profile?.User?.githubUsername
+                  }
+                  alt="profile"
+                  width={100}
+                  height={100}
+                  className="rounded-full"
+                />
+              </div>
+              <div className="text-center text-lg mt-2 flex flex-col">
+                {props?.profile?.User?.githubUsername}
+                <span>#{props?.rank}</span>
+              </div>
+            </div>
+            <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default RankCard;
