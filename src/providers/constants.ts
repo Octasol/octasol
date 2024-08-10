1bb8f30eff3f9bb0dec5981b7f7c906933db72b2
mayank-dev07 <devmayank.hts@gmail.com>
2024-08-10 17:41:39 +0530

    refact: const fiel for providers created

--- src/providers/constants.ts
+++ src/providers/constants.ts
@@ -1,16 +1,14 @@
 const SuperteamEarn = "173b32e0-a611-42ad-9cb7-ab4029c0a72a";
 const Leetcode = "";
-const Github = "";
 const Codechef = "";
 const Codeforces = "";
+const Gitlab = "";
 const hackerrank = "";
 const Geeksforgeeks = "";
-const Gitlab = "";
 
 export const providers: any = {
   SuperteamEarn,
   Leetcode,
-  Github,
   Codechef,
   Codeforces,
   hackerrank,
