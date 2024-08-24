c0ce9dbfca81c6cb246d1339672ee5261dedc3ff
A91y <ayush.agr254@gmail.com>
2024-08-24 23:16:15 +0530

    fix: param empty error

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -2,7 +2,7 @@ import { bigintToString } from "@/lib/utils";
 import { getDbUser } from "@/utils/dbUtils";
 import { NextResponse } from "next/server";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(request: Request, { params }: { params: { id: string } }) {
   try {
     const { id } = params;
 
