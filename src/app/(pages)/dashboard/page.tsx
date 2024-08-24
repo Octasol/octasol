2cc0dea25e70a0775e07e07daa35daad4add737a
mayank-dev07 <devmayank.hts@gmail.com>
2024-08-25 03:37:16 +0530

    fix dfashboard code reverted

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,87 +1,76 @@
-"use client";
-import React, { useState } from "react";
-import { Input } from "@/components/ui/input";
-import { POST } from "@/config/axios/requests";
-import { useSelector } from "react-redux";
-import { useSession } from "next-auth/react";
+import React from "react";
 
 const Dashboard = () => {
-  const [email, setEmail] = useState("");
-  const [otp, setOtp] = useState("");
-  // const [loading, setLoading] = useState(false);
-  // const [error, setError] = useState("");
-  // const [success, setSuccess] = useState("");
-  const { data: session } = useSession() as any;
-  const sendEmail = async () => {
-   
-    try {
-      const response = await POST(
-        "/auth/send-otp",
-        { email },
-        {
-          Authorization: `Bearer ${session.accessToken as string}`,
-        }
-      );
-      // setSuccess(
-      //   "Email sent successfully. Please check your inbox for the OTP."
-      // );
-    } catch (err) {
-      // setError("Failed to send email. Please try again.");
-    } finally {
-      // setLoading(false);
-    }
-  };
-
-  const verifyOtp = async () => {
-   
-    try {
-      const response = await POST(
-        "/auth/verify-otp",
-        { email, otp },
-        {
-          Authorization: `Bearer ${session.accessToken as string}`,
-        }
-      );
-      // setSuccess("OTP verified successfully!");
-    } catch (err) {
-      // setError("Failed to verify OTP. Please try again.");
-    } finally {
-      // setLoading(false);
-    }
-  };
-
   return (
     <div className="flex flex-col gap-4 h-full w-full justify-center items-center">
-      <h1 className="text-xl md:text-3xl px-8">Dashboard</h1>
-      <div className="overflow-auto h-[80vh] pb-14 md:pb-8 px-8 py-8">
-        <Input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button
-          onClick={()=>sendEmail()}
-          className="btn-primary mt-4"
-        >
-          {/* {loading ? "Sending..." : ""} */}Send Otp
-        </button>
-        <Input
-          type="text"
-          placeholder="OTP"
-          value={otp}
-          onChange={(e) => setOtp(e.target.value)}
-          className="mt-4"
-        />
-        <button
-          onClick={()=>verifyOtp()}
-          // disabled={loading}
-          className="btn-primary mt-4"
-        >
-          {/* {loading ? "Verifying..." : ""} */}Verify OTP
-        </button>
-        {/* {error && <div className="text-red-500 mt-4">{error}</div>}
-        {success && <div className="text-green-500 mt-4">{success}</div>} */}
+      <h1 className="text-xl md:text-3xl px-8 ">Dashboard</h1>
+      <div className=" overflow-auto h-[80vh]  pb-14 md:pb-8 px-8 py-8">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, quia
+        voluptatibus? Expedita quos, nisi iste aspernatur est omnis quibusdam
+        minima non laboriosam voluptatum recusandae nihil quia dolorem placeat.
+        Asperiores vitae necessitatibus fugit exercitationem quam placeat minus
+        libero dolor veritatis provident qui adipisci nulla aspernatur
+        laboriosam, laudantium, optio animi, maxime non. Esse quo sed optio
+        eaque. Consequuntur neque numquam molestiae commodi quis reprehenderit
+        tenetur, hic nihil aperiam. Hic illo eos perspiciatis, commodi
+        repellendus id! Ducimus quidem suscipit similique et inventore incidunt
+        expedita blanditiis dolorum, saepe excepturi architecto id corporis rem
+        praesentium dolor! Quasi vitae exercitationem dolorem earum veritatis.
+        Praesentium molestias nobis consectetur adipisci, amet totam tenetur,
+        odio officia maiores quo sint facilis nemo quisquam ad magnam quia
+        fugit. Recusandae voluptate eveniet facilis repellendus quis beatae est
+        dolorum, atque illo expedita aliquid, possimus nobis aliquam excepturi
+        molestias, quia unde tenetur? Ipsam, quisquam? Assumenda illum dolor
+        doloribus tempora. Ratione dicta libero recusandae in id, ipsam, fugiat
+        numquam inventore totam deleniti omnis, sapiente minus commodi ex eum
+        exercitationem placeat? Voluptatibus, illo doloremque porro atque quasi
+        dolorum itaque eveniet ex assumenda voluptatum, libero saepe accusamus
+        facilis. Ipsam maxime, molestias quos possimus aspernatur quo iste
+        quibusdam nisi qui impedit debitis illum perspiciatis esse accusamus
+        corrupti obcaecati vel explicabo nihil est ad provident eaque soluta
+        quasi. Tempore architecto totam ipsum voluptatem! Cupiditate adipisci
+        similique explicabo cum fugit quis animi maiores, ipsum beatae
+        voluptatibus, dolorem quidem necessitatibus fuga, odit doloremque
+        dolores itaque veniam sint debitis harum sequi. Corrupti commodi impedit
+        rem, possimus sed quidem itaque, laborum ab aliquid illo id dolores
+        facere suscipit nihil modi voluptates, cupiditate molestiae ex vero
+        earum amet eveniet voluptatibus iste quam! Voluptatum accusantium neque
+        voluptatem odio! Dicta explicabo numquam dignissimos, perspiciatis
+        recusandae, facilis adipisci modi dolores tenetur ex obcaecati. Neque
+        earum repudiandae laboriosam itaque vero maxime expedita consectetur
+        dolor vel debitis tenetur ea magni perspiciatis, aut blanditiis, impedit
+        assumenda omnis totam voluptatum? Dolorum itaque eos ut earum, molestias
+        aut quos corrupti quaerat, minima repellat est sit. Cumque sunt quod
+        corrupti. Ea blanditiis magni repellat quis laborum? Magnam dolore harum
+        dolor minus facere voluptatibus doloremque beatae incidunt qui,
+        consectetur et, cupiditate consequatur velit in possimus natus,
+        laboriosam nobis. Quisquam nihil beatae dolores, temporibus dolorem,
+        itaque enim eum illo explicabo reprehenderit corrupti molestiae
+        consequuntur sit aliquam libero harum culpa, numquam natus aliquid neque
+        accusamus cupiditate! Iste alias veniam quia hic consectetur est laborum
+        dolorum dolor placeat nulla sapiente eos optio ipsam odio, incidunt
+        itaque expedita repellendus natus omnis labore nisi doloremque velit
+        rerum illo. Optio consequuntur nam suscipit, sit atque ullam! Eveniet
+        iusto fuga commodi quod molestias voluptatibus nulla voluptatum corrupti
+        dolorem reiciendis sint saepe perferendis et dolore hic explicabo nobis,
+        autem atque impedit. Illum numquam aliquam doloremque odit iste facilis
+        doloribus debitis? Numquam aspernatur ipsam ab ullam enim, nulla
+        voluptatem ratione sit eos. Vel nihil suscipit delectus obcaecati
+        necessitatibus minus ex repellendus voluptates, odio corrupti dicta
+        praesentium enim nesciunt odit quos illo excepturi dolore quo.
+        Necessitatibus corporis numquam nobis deleniti dicta! Voluptate illum
+        magni blanditiis iste dolore quae reiciendis nam vitae culpa aperiam
+        corporis suscipit nulla non placeat sit perferendis voluptatem dolores
+        tempora eveniet, optio laborum dicta? Unde blanditiis vitae iure. Alias
+        maiores iure molestiae, veniam laudantium fugit rem quisquam nihil, quod
+        aliquam ad doloremque voluptatibus! Eius praesentium nemo rerum
+        asperiores fugiat enim aliquid maiores sapiente assumenda commodi amet
+        ipsa cum quae pariatur quam odit, vitae nulla temporibus maxime tempora.
+        Ut, magnam exercitationem molestias enim perspiciatis amet recusandae
+        veritatis omnis odit laborum non voluptas fuga sit reiciendis quo
+        numquam! Beatae, unde? Quibusdam sint, iusto error, harum enim veniam
+        architecto iste eaque perferendis quas optio, blanditiis maiores
+        reprehenderit quis debitis nobis distinctio earum numquam eius?
       </div>
     </div>
   );
