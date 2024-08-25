54d82f19e3029e9bd94105524cb3f875f947399d
mayank-dev07 <devmayank.hts@gmail.com>
2024-08-25 16:57:15 +0530

    feat: profile page added to sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,16 +1,22 @@
 import { IconChartHistogram } from "@tabler/icons-react";
-import { Blocks, CopyPlus, HomeIcon } from "lucide-react";
+import { Blocks, CopyPlus, HomeIcon, User } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import { useSelector } from "react-redux";
 
 type Props = {};
 
 const Sidebar = (props: Props) => {
+  const user = useSelector((state: any) => state.user);
+
   return (
     <>
       <div className="min-w pt-24 pb-4 min-h-screen z-50 overflow-hidden fixed flex flex-col items-center gap-8 px-5">
         <Link href="/dashboard">
           <HomeIcon size={32} />
+        </Link>
+        <Link href={`/p/${user?.login}`}>
+          <User size={32} />
         </Link>
         <Link href="/repoinitialize">
           <CopyPlus size={32} />
