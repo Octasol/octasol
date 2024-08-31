2256730efdbb0af1e044b2e8cfc6caa867955ce8
mayank-dev07 <devmayank.hts@gmail.com>
2024-08-31 22:29:31 +0530

    fix: cover page edited

--- src/components/CoverUpPage/Index.tsx
+++ src/components/CoverUpPage/Index.tsx
@@ -5,8 +5,8 @@ type Props = {};
 const CoverUpPage = (props: Props) => {
   return (
     <>
-      <div className="w-full h-full bg-transparent backdrop-blur-md z-50 absolute top-0 right-0">
-        <div className="flex justify-center items-center h-full w-full text-4xl">
+      <div className="w-full h-full bg-transparent backdrop-blur-md z-30 absolute top-0 right-0 ">
+        <div className="flex justify-center items-center h-screen w-full text-4xl overflow-hidden">
           Coming Soon
         </div>
       </div>
