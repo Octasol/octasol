d7647ba5e572d65c587a5c5339df33e696cc0744
mayank-dev07 <devmayank.hts@gmail.com>
2024-08-31 22:03:28 +0530

    feat: coverup page added to repoinitialize page

--- src/app/(pages)/repoinitialize/page.tsx
+++ src/app/(pages)/repoinitialize/page.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import { RepoInitializeForm } from "@/components/RepoInitializeForm";
 import { Meteors } from "@/components/ui/metors";
 import { astronautIcon } from "@/components/Svg/svg";
+import CoverUpPage from "@/components/CoverUpPage/Index";
 
 const Repoinitialize = () => {
   return (
-    <div className="flex flex-col gap-4 h-full w-full justify-center items-center relative z-10">
-      <div className=" pb-14 lg:pb-8 px-8 py-8 w-full flex flex-col lg:flex-row">
-        <Meteors number={50} className="z-0" />
-        <div className="w-full lg:w-1/2 justify-center items-center z-20">
-          <RepoInitializeForm />
+    <>
+      <div className="flex flex-col gap-4 h-full w-full justify-center items-center relative z-10">
+        <CoverUpPage />
+        <div className=" pb-14 lg:pb-8 px-8 py-8 w-full flex flex-col lg:flex-row">
+          <Meteors number={50} className="z-0" />
+          <div className="w-full lg:w-1/2 justify-center items-center z-20">
+            <RepoInitializeForm />
+          </div>
+          <div className="w-full lg:w-1/2 hidden lg:flex">
+            {astronautIcon()}
+          </div>
         </div>
-        <div className="w-full lg:w-1/2 hidden lg:flex">{astronautIcon()}</div>
       </div>
-    </div>
+    </>
   );
 };
 
