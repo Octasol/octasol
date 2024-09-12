21a9aa0a0d56e39257f4c8daff00c98c20cab08c
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-09-12 09:19:50 +0530

    refactor: updated reclaim js sdk and leetcode service

--- src/config/reclaim/leetcode/service.ts
+++ src/config/reclaim/leetcode/service.ts
@@ -1,14 +1,12 @@
-
 export async function processLeetcodeData(
-    githubId: string,
-    proof: any,
-    providerName: string
-  ) {
-    const username = JSON.parse(proof[0].claimData.context).extractedParameters
-      .username;
-    console.log(username)
-    const lastUpdateTimeStamp = proof[0].claimData.timestampS;
-    console.log("proof is:", proof[0])
-    return true;
-  }
-  
+  githubId: string,
+  proof: any,
+  providerName: string
+) {
+  const username = JSON.parse(proof[0].claimData.context).extractedParameters
+    .username;
+  console.log(username);
+  const lastUpdateTimeStamp = proof[0].claimData.timestampS;
+  console.log("proof is:", proof[0]);
+  return true;
+}
