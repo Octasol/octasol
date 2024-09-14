0aa41627389596631c912f1dcc6b6eda09565b07
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-09-14 14:55:19 +0530

    feat: added codechef api

--- src/config/reclaim/codechef/service.ts
+++ src/config/reclaim/codechef/service.ts
@@ -1,13 +1,25 @@
+import axios from "axios";
+
 export async function processCodechefData(
-    githubId: string,
-    proof: any,
-    providerName: string
-  ) {
-    const username = JSON.parse(proof[0].claimData.context).extractedParameters
-      .URL_PARAMS_GRD;
-    console.log(username)
-    const lastUpdateTimeStamp = proof[0].claimData.timestampS;
-    console.log("proof is:", proof[0])
+  githubId: string,
+  proof: any,
+  providerName: string
+) {
+  const username = JSON.parse(proof[0].claimData.context).extractedParameters
+    .URL_PARAMS_GRD;
+  console.log("CodeChef Username:", username);
+
+  const lastUpdateTimeStamp = proof[0].claimData.timestampS;
+  console.log("Proof is:", proof[0]);
+
+  try {
+    const response = await axios.get(`https://codechef-api.vercel.app/handle/${username}`);
+    
+    console.log("CodeChef API Response:", response.data);
+
     return true;
+  } catch (error) {
+    console.error(`Failed to fetch CodeChef data for username: ${username}`, error);
+    throw new Error("Error fetching CodeChef data");
   }
-  
+}
