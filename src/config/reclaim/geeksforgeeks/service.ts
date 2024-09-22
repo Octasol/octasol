942167ffbf72e0a461a6ca25ac4e3f20845b4e0a
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-09-23 04:01:22 +0530

    fix: made gfg data set in db

--- src/config/reclaim/geeksforgeeks/service.ts
+++ src/config/reclaim/geeksforgeeks/service.ts
@@ -5,11 +5,11 @@ export async function processGeeksForGeeksData(
   proof: any,
   providerName: string
 ) {
-  const score = JSON.parse(proof[0].claimData.context).extractedParameters
-    .score;
+  const score = parseInt(JSON.parse(proof[0].claimData.context).extractedParameters
+    .score);
   console.log(score);
-  const problemsSolved = JSON.parse(proof[0].claimData.context)
-    .extractedParameters.total_problems_solved;
+  const problemsSolved = parseInt(JSON.parse(proof[0].claimData.context)
+    .extractedParameters.total_problems_solved);
   console.log(problemsSolved);
   const username = JSON.parse(proof[0].claimData.context).extractedParameters
     .URL_PARAMS_1;
