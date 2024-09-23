615a834e0576854beccab238c3d38a696345c102
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-09-23 19:40:02 +0530

    feat: setting username for earn done

--- src/config/reclaim/superteamearn/service.ts
+++ src/config/reclaim/superteamearn/service.ts
@@ -1,26 +1,29 @@
-import { scrapeSuperteamStats } from './scraper';
+import { scrapeSuperteamStats } from "./scraper";
+import { setUsername } from "@/utils/dbUtils";
 
 export async function processSuperteamEarnData(
-  githubId: string,
+  githubId: any,
   proof: any,
   providerName: string
 ) {
   const username = JSON.parse(proof[0].claimData.context).extractedParameters
     .username;
-  console.log('Username:', username);
+  await setUsername(githubId, {
+    superteamUsername: username,
+  });
 
   const stats = await scrapeSuperteamStats(username);
 
   if (stats) {
-    console.log('Participations:', stats.participations);
-    console.log('Wins:', stats.wins);
-    console.log('Total Winnings:', stats.totalWinnings);
+    console.log("Participations:", stats.participations);
+    console.log("Wins:", stats.wins);
+    console.log("Total Winnings:", stats.totalWinnings);
   } else {
-    console.log('Failed to fetch Superteam stats.');
+    console.log("Failed to fetch Superteam stats.");
   }
 
   const lastUpdateTimeStamp = proof[0].claimData.timestampS;
-  console.log('Proof is:', proof[0]);
+  console.log("Proof is:", proof[0]);
 
   return true;
 }
