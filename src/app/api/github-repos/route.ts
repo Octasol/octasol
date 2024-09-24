71c02bafef74342a5bcebd5d382a25f2e02a3d66
A91y <ayush.agr254@gmail.com>
2024-09-24 16:09:49 +0530

    refactor: remove unused environment variables

--- src/app/api/github-repos/route.ts
+++ src/app/api/github-repos/route.ts
@@ -2,6 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 import { getAccessToken } from "@/lib/apiUtils";
 
+/**
+ * 
+ * @param req NextRequest
+ * @returns Next JSON response
+ * @note This function required env variables like GITHUB_PRIVATE_KEY_FILE_NAME & GITHUB_APP_ID
+ */
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const installationId = searchParams.get("installationId");
