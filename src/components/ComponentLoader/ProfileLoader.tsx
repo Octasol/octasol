1d2d9ebfa899ce49d241c21ba2761aaeaccce933
mayank-dev07 <devmayank.hts@gmail.com>
2024-09-26 01:37:38 +0530

    refact: profile loader updated

--- src/components/ComponentLoader/ProfileLoader.tsx
+++ src/components/ComponentLoader/ProfileLoader.tsx
@@ -2,12 +2,18 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export function ProfileLoader() {
   return (
-    <div className="flex items-center space-x-4">
-      <Skeleton className="h-12 w-12 rounded-full" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
-      </div>
-    </div>
+    <>
+      {Array(4)
+        .fill(null)
+        .map((_, index) => (
+          <div key={index} className="flex items-center space-x-4 py-3">
+            <Skeleton className="h-12 w-12 rounded-full" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+        ))}
+    </>
   );
 }
