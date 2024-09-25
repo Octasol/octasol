7f45740990b280d3d39111a138996f6b0a47f577
mayank-dev07 <devmayank.hts@gmail.com>
2024-09-25 23:07:33 +0530

    fix: beta version added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,13 +8,16 @@ const Header = (props: Props) => {
   return (
     <>
       <nav className="text-white fixed top-0 right-0 left-0 w-full h-20 flex justify-between items-center md:px-20 px-2 py-6 bg-black z-50 ">
-        <Image
-          src="/octasolLandingLogo.png"
-          alt="logo"
-          width={80}
-          height={80}
-          loading="lazy"
-        />
+        <div className="flex justify-center items-center">
+          <Image
+            src="/octasolLandingLogo.png"
+            alt="logo"
+            width={80}
+            height={80}
+            loading="lazy"
+          />
+          <span className="text-sm font-bold tracking-widest">Beta</span>
+        </div>
         <div className="flex items-center gap-6">
           <Login />
         </div>
