21fc5d149b8d7c50607b98a26b8e0348bed6979f
A91y <ayush.agr254@gmail.com>
2024-09-27 15:35:01 +0530

    feat: update session max age in auth config

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -38,6 +38,9 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
+  session: {
+    maxAge: 10 * 24 * 60 * 60,
+  },
 };
 
 async function getEmail(authToken: string) {
