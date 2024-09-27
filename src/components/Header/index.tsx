8d827a70300c05a9a35e4dd4c56fa76d8cb0c875
A91y <ayush.agr254@gmail.com>
2024-09-27 15:49:16 +0530

    refactor: update Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import Login from "../Login/Login";
 
@@ -9,14 +10,16 @@ const Header = (props: Props) => {
     <>
       <nav className="text-white fixed top-0 right-0 left-0 w-full h-20 flex justify-between items-center md:px-20 px-2 py-6 bg-black z-50 ">
         <div className="flex justify-center items-center">
-          <Image
-            src="/octasolLandingLogo.png"
-            alt="logo"
-            width={80}
-            height={80}
-            loading="lazy"
-            priority={false}
-          />
+          <Link href="/" passHref>
+            <Image
+              src="/octasolLandingLogo.png"
+              alt="logo"
+              width={80}
+              height={80}
+              loading="lazy"
+              priority={false}
+            />
+          </Link>
           <span className="text-sm font-bold tracking-widest">Beta</span>
         </div>
         <div className="flex items-center gap-6">
