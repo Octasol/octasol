7bb46d7eaf56d06241f3c81b5a97e7e4dc1fb4de
A91y <ayush.agr254@gmail.com>
2024-09-27 16:03:25 +0530

    refactor: update caching headers in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/assets/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable", // Cache for 1 year
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
