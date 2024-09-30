c38714d2265efb25f4d05ff42265dd3f0e6edc7d
mayank-dev07 <devmayank.hts@gmail.com>
2024-10-01 00:02:07 +0530

    fix: removed noStore

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -2,8 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getAllProfiles } from "@/utils/dbUtils";
 import { unstable_noStore as noStore } from "next/cache";
 
-export async function GET() {
-  noStore();
+export async function GET(req: NextRequest) {
   try {
     const allProfiles = await getAllProfiles();
     if (!allProfiles) {
@@ -21,6 +20,7 @@ export async function GET() {
     const response = NextResponse.json(serializedProfile);
     return response;
   } catch (error: any) {
+    console.error(error);
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
