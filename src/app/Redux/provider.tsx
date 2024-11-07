365fea173c4f15045e8c8b1a7c0029036ce154f3
mayank-dev07 <devmayank.hts@gmail.com>
2024-11-07 16:29:27 +0530

    feat: redux persist added

--- src/app/Redux/provider.tsx
+++ src/app/Redux/provider.tsx
@@ -1,9 +1,17 @@
 "use client";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { persistor, store } from "./store";
 import { ReactNode } from "react";
+import { PersistGate } from "redux-persist/integration/react";
+
 type Props = { children: ReactNode };
 
 export function Providers({ children }: Props) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
