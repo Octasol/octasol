7f7b41d2fa5ab9ac880fb84283ae14ecbb2918bc
A91y <ayush.agr254@gmail.com>
2024-11-17 15:57:01 +0530

    Add support for priority queues in task processing

--- src/lib/queue.ts
+++ src/lib/queue.ts
@@ -1,14 +1,25 @@
 import { Queue } from "bullmq";
 import Redis from "ioredis";
+import { QueuePriority } from "./types";
 
 const redisConnection = new Redis(process.env.REDIS_URL || "");
 
-// Initialize the queue
-export const taskQueue = new Queue("taskQueue", {
+const highPriorityQueueName = "highPriorityQueue";
+const lowPriorityQueueName = "lowPriorityQueue";
+
+// Initialize the high priority queue
+const highPriorityQueue = new Queue(highPriorityQueueName, {
   connection: redisConnection,
 });
 
-// Function to add tasks
-export const addToQueue = async (taskData: object) => {
-  await taskQueue.add("task", taskData);
+// Initialize the low priority queue
+const lowPriorityQueue = new Queue(lowPriorityQueueName, {
+  connection: redisConnection,
+});
+
+// Function to add tasks to the appropriate queue
+export const addToQueue = async (taskData: object, priority: QueuePriority) => {
+  const queue =
+    priority === QueuePriority.High ? highPriorityQueue : lowPriorityQueue;
+  await queue.add("task", taskData);
 };
