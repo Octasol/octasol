7f646ead845ea584ff90bec8d4a769dcdd4ff323
mayank-dev07 <devmayank.hts@gmail.com>
2024-12-09 10:57:36 +0530

    fix: cover bg blur in safari test2-s

--- src/components/CoverUpPage/Index.tsx
+++ src/components/CoverUpPage/Index.tsx
@@ -7,7 +7,7 @@ type Props = {};
 const CoverUpPage = (props: Props) => {
   return (
     <>
-      <div className="w-full h-full bg-transparent blur z-30 fixed top-0 right-0 text-white">
+      <div className="w-full h-full bg-transparent cover z-30 fixed top-0 right-0 text-white">
         <div className="flex flex-col justify-center items-center h-screen w-full  overflow-hidden">
           <p className="text-4xl md:text-5xl lg:text-7xl">Coming Soon</p>
           <p className="mt-8 text-center text-lg tracking-wide leading-8">
