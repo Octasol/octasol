ed6ca135c8edc317f6c6712f0bceefd425304273
mayank-dev07 <devmayank.hts@gmail.com>
2024-12-12 12:51:49 +0530

    fix: dashboard card ui-s

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -8,10 +8,10 @@ const Dashboard = () => {
   return (
     <>
       <div className=" overflow-auto flex flex-col gap-4 h-full w-full items-center relative">
-        <CoverUpPage />
+        {/* <CoverUpPage /> */}
         <h1 className="text-xl md:text-3xl px-8">Dashboard</h1>
         <div className="pb-10 md:pb-4 px-8  w-full">
-          <div className="flex flex-wrap justify-evenly w-full gap-2">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-evenly w-full gap-5">
             <Cards name="Bounties" data="100" />
             <Cards name="Participations" data="300" />
             <Cards name="Wins" data="10" />
