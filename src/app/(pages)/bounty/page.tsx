d697e7f71c902811dd410369db86a31314079987
mayank-dev07 <devmayank.hts@gmail.com>
2024-12-13 19:55:14 +0530

    refact: bounty page card component updated-s

--- src/app/(pages)/bounty/page.tsx
+++ src/app/(pages)/bounty/page.tsx
@@ -53,26 +53,44 @@ const Bounty = () => {
           {bounties.map((bounty, index) => (
             <div
               key={index}
-              className="relative inline-flex h-full overflow-hidden rounded-lg shadow-md shadow-[#34597f]"
+              className="relative inline-flex h-full overflow-hidden rounded-lg shadow-md shadow-[#34597f] cursor-pointer"
+              onClick={() => bountyDetails(bounty.id)}
             >
               <div className="w-full p-5 flex flex-col justify-between gap-3">
                 <div className=" rounded-lg  w-full gap-4 flex flex-col ">
-                  <h3 className="text-sm font-semibold text-gray-400 ">
-                    {bounty?.sponsor?.name}
-                  </h3>
+                  <div className="flex justify-between items-center">
+                    {/* <h3 className="text-sm font-semibold text-gray-300 underline underline-offset-4">
+                      Sponsor
+                    </h3> */}
+                    {bounty?.sponsor?.image ? (
+                      <Image
+                        src={bounty?.sponsor?.image}
+                        alt={bounty?.sponsor?.name}
+                        width={100}
+                        height={100}
+                      />
+                    ) : (
+                      <User size={50} />
+                    )}
+                    <h3 className="text-sm font-bold text-gray-300">
+                      {bounty?.sponsor?.name}
+                    </h3>
+                  </div>
                   <div className="flex justify-between items-center">
                     <h1 className="text-lg font-bold ">{bounty?.bountyname}</h1>
                     <p className="font-bold">$&nbsp;{bounty.price}</p>
                   </div>
-                  <p className="text-sm text-slate-500 italic truncate">
+                  {/* <p className="text-sm text-slate-400 italic truncate">
                     {bounty?.sponsor?.description}
-                  </p>
+                  </p> */}
                   <div className="flex flex-col justify-between gap-4">
-                    <p className="font-bold">Skills Required</p>
+                    {/* <p className="font-bold text-gray-300 underline underline-offset-4">
+                      Skills Required
+                    </p> */}
                     <div className="flex flex-wrap gap-2 ">
                       {bounty.skills.map((skill) => (
                         <div key={index}>
-                          <button className="px-4 py-2 text-sm font-semibold border border-gray-800 rounded-lg hover:bg-gray-900 cursor-default">
+                          <button className="px-4 py-2 text-sm font-semibold border border-gray-800 rounded-lg ">
                             {skill}
                           </button>
                         </div>
@@ -82,12 +100,14 @@ const Bounty = () => {
                 </div>
 
                 <div className="flex flex-col">
-                  <div className="flex justify-between items-center">
-                    <p className="font-bold">Hunters Applied</p>
+                  {/* <div className="flex justify-between items-center">
+                    <p className="font-bold text-gray-300 underline underline-offset-4">
+                      Hunters Applied
+                    </p>
                     <p className="font-bold">{bounty.submissions.length}</p>
-                  </div>
+                  </div> */}
                   <div className="flex items-center justify-between mt-4 ">
-                    <p className="text-xs text-gray-400 ">
+                    <p className="text-xs text-gray-300 ">
                       {new Date(bounty?.createdAt).toLocaleDateString("en-US", {
                         day: "numeric",
                         month: "short",
@@ -98,7 +118,7 @@ const Bounty = () => {
                       className="px-4 py-2 text-sm font-semibold border border-gray-800 rounded-lg hover:bg-gray-900"
                       onClick={() => bountyDetails(bounty.id)}
                     >
-                      Details
+                      Apply
                     </button>
                   </div>
                 </div>
