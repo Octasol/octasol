a0a8bc02b9dfc42805d59b472e746f21c9327649
mayank-dev07 <devmayank.hts@gmail.com>
2024-12-26 18:08:24 +0530

    refact: removed useless imports

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -6,7 +6,6 @@ import {
   CopyPlus,
   HomeIcon,
   User,
-  PictureInPicture,
 } from "lucide-react";
 import Link from "next/link";
 import React from "react";
@@ -14,7 +13,6 @@ import { useSelector } from "react-redux";
 import {
   Tooltip,
   TooltipContent,
-  TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { usePathname } from "next/navigation";
@@ -39,131 +37,128 @@ const Sidebar = ({ verified }: Props) => {
             verified ? "z-50" : "z-0 hidden"
           )}
         >
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger>
-                <Link
-                  href="/dashboard"
-                  className="flex items-center gap-4 hover:text-[#45bd95]"
+          <Tooltip>
+            <TooltipTrigger>
+              <Link
+                href="/dashboard"
+                className="flex items-center gap-4 hover:text-[#45bd95]"
+              >
+                <HomeIcon
+                  size={32}
+                  color={isActive("/dashboard") ? "cyan" : "currentColor"}
+                />
+
+                <span
+                  className={`hidden group-hover:inline-block transition-all duration-300 ease-in-out  ${
+                    isActive("/dashboard") && "text-cyan-500"
+                  }`}
                 >
-                  <HomeIcon
-                    size={32}
-                    color={isActive("/dashboard") ? "cyan" : "currentColor"}
-                  />
+                  Dashboard
+                </span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent className="bg-black">
+              <p>Dashboard</p>
+            </TooltipContent>
+          </Tooltip>
 
-                  <span
-                    className={`hidden group-hover:inline-block transition-all duration-300 ease-in-out  ${
-                      isActive("/dashboard") && "text-cyan-500"
-                    }`}
-                  >
-                    Dashboard
-                  </span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent className="bg-black">
-                <p>Dashboard</p>
-              </TooltipContent>
-            </Tooltip>
-
-            <Tooltip>
-              <TooltipTrigger>
-                <Link
-                  href={`/p/${user?.login}`}
-                  className="flex items-center gap-4 hover:text-[#45bd95]"
+          <Tooltip>
+            <TooltipTrigger>
+              <Link
+                href={`/p/${user?.login}`}
+                className="flex items-center gap-4 hover:text-[#45bd95]"
+              >
+                <User
+                  size={32}
+                  color={isActive("/p/") ? "cyan" : "currentColor"}
+                />
+                <span
+                  className={`hidden group-hover:inline-block transition-all duration-300 ease-in-out  ${
+                    isActive("/p/") && "text-cyan-500"
+                  }`}
                 >
-                  <User
-                    size={32}
-                    color={isActive("/p/") ? "cyan" : "currentColor"}
-                  />
-                  <span
-                    className={`hidden group-hover:inline-block transition-all duration-300 ease-in-out  ${
-                      isActive("/p/") && "text-cyan-500"
-                    }`}
-                  >
-                    Profile
-                  </span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent className="bg-black">
-                <p>Profile</p>
-              </TooltipContent>
-            </Tooltip>
+                  Profile
+                </span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent className="bg-black">
+              <p>Profile</p>
+            </TooltipContent>
+          </Tooltip>
 
-            <Tooltip>
-              <TooltipTrigger>
-                <Link
-                  href="/repoinitialize"
-                  className="flex items-center gap-4 hover:text-[#45bd95]"
+          <Tooltip>
+            <TooltipTrigger>
+              <Link
+                href="/repoinitialize"
+                className="flex items-center gap-4 hover:text-[#45bd95]"
+              >
+                <CopyPlus
+                  size={32}
+                  color={isActive("/repoinitialize") ? "cyan" : "currentColor"}
+                />
+                <span
+                  className={`hidden group-hover:inline-block transition-all duration-300 ease-in-out  ${
+                    isActive("/repoinitialize") && "text-cyan-500"
+                  }`}
                 >
-                  <CopyPlus
-                    size={32}
-                    color={
-                      isActive("/repoinitialize") ? "cyan" : "currentColor"
-                    }
-                  />
-                  <span
-                    className={`hidden group-hover:inline-block transition-all duration-300 ease-in-out  ${
-                      isActive("/repoinitialize") && "text-cyan-500"
-                    }`}
-                  >
-                    Repository
-                  </span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent className="bg-black">
-                <p>Repository</p>
-              </TooltipContent>
-            </Tooltip>
+                  Repository
+                </span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent className="bg-black">
+              <p>Repository</p>
+            </TooltipContent>
+          </Tooltip>
 
-            <Tooltip>
-              <TooltipTrigger>
-                <Link
-                  href="/connect"
-                  className="flex items-center gap-4 hover:text-[#45bd95]"
+          <Tooltip>
+            <TooltipTrigger>
+              <Link
+                href="/connect"
+                className="flex items-center gap-4 hover:text-[#45bd95]"
+              >
+                <Blocks
+                  size={32}
+                  color={isActive("/connect") ? "cyan" : "currentColor"}
+                />
+                <span
+                  className={`hidden group-hover:inline-block transition-all duration-300 ease-in-out  ${
+                    isActive("/connect") && "text-cyan-500"
+                  }`}
                 >
-                  <Blocks
-                    size={32}
-                    color={isActive("/connect") ? "cyan" : "currentColor"}
-                  />
-                  <span
-                    className={`hidden group-hover:inline-block transition-all duration-300 ease-in-out  ${
-                      isActive("/connect") && "text-cyan-500"
-                    }`}
-                  >
-                    Connect
-                  </span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent className="bg-black">
-                <p>Connect</p>
-              </TooltipContent>
-            </Tooltip>
+                  Connect
+                </span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent className="bg-black">
+              <p>Connect</p>
+            </TooltipContent>
+          </Tooltip>
 
-            <Tooltip>
-              <TooltipTrigger>
-                <Link
-                  href="/leaderboard"
-                  className="flex items-center gap-4 hover:text-[#45bd95]"
+          <Tooltip>
+            <TooltipTrigger>
+              <Link
+                href="/leaderboard"
+                className="flex items-center gap-4 hover:text-[#45bd95]"
+              >
+                <Trophy
+                  size={32}
+                  color={isActive("/leaderboard") ? "cyan" : "currentColor"}
+                />
+                <span
+                  className={`hidden group-hover:inline-block transition-all duration-300 ease-in-out  ${
+                    isActive("/leaderboard") && "text-cyan-500"
+                  }`}
                 >
-                  <Trophy
-                    size={32}
-                    color={isActive("/leaderboard") ? "cyan" : "currentColor"}
-                  />
-                  <span
-                    className={`hidden group-hover:inline-block transition-all duration-300 ease-in-out  ${
-                      isActive("/leaderboard") && "text-cyan-500"
-                    }`}
-                  >
-                    Leaderboard
-                  </span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent className="bg-black">
-                <p>Leaderboard</p>
-              </TooltipContent>
-            </Tooltip>
+                  Leaderboard
+                </span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent className="bg-black">
+              <p>Leaderboard</p>
+            </TooltipContent>
+          </Tooltip>
 
-            {/* <Tooltip>
+          {/* <Tooltip>
               <TooltipTrigger>
                 <Link
                   href="/listunescrowed"
@@ -189,30 +184,29 @@ const Sidebar = ({ verified }: Props) => {
               </TooltipContent>
             </Tooltip> */}
 
-            <Tooltip>
-              <TooltipTrigger>
-                <Link
-                  href="/bounty"
-                  className="flex items-center gap-4 hover:text-[#45bd95]"
+          <Tooltip>
+            <TooltipTrigger>
+              <Link
+                href="/bounty"
+                className="flex items-center gap-4 hover:text-[#45bd95]"
+              >
+                <BadgeDollarSign
+                  size={32}
+                  color={isActive("/bounty") ? "cyan" : "currentColor"}
+                />
+                <span
+                  className={`hidden group-hover:inline-block transition-all duration-300 ease-in-out  ${
+                    isActive("/bounty") && "text-cyan-500"
+                  }`}
                 >
-                  <BadgeDollarSign
-                    size={32}
-                    color={isActive("/bounty") ? "cyan" : "currentColor"}
-                  />
-                  <span
-                    className={`hidden group-hover:inline-block transition-all duration-300 ease-in-out  ${
-                      isActive("/bounty") && "text-cyan-500"
-                    }`}
-                  >
-                    Bounty
-                  </span>
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent className="bg-black">
-                <p>Bounty</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+                  Bounty
+                </span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent className="bg-black">
+              <p>Bounty</p>
+            </TooltipContent>
+          </Tooltip>
         </div>
 
         <div className="rotate-180 h-screen w-px">
