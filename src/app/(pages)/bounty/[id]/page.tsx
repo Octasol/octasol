917db5f2c1a2c7972a10c755b88fd9b45bf02ca8
mayank-dev07 <devmayank.hts@gmail.com>
2024-12-26 17:53:42 +0530

    refact: update bounty page submission button text

--- src/app/(pages)/bounty/[id]/page.tsx
+++ src/app/(pages)/bounty/[id]/page.tsx
@@ -358,7 +358,11 @@ const page = () => {
                           }
                         }}
                         className="relative inline-flex items-center justify-center p-0.5  overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 cursor-pointer opacity-50"
-                      ></button>
+                      >
+                        <span className="relative px-3 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-60">
+                          {"Submit\u00A0Application"}
+                        </span>
+                      </button>
                     </div>
                   )}
                 </div>
